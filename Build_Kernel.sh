1bc5ebbfe3800d2f6698917ee8d80b896cdfc5cd
IDhiding <24775926+IDhiding@users.noreply.github.com>
2025-06-01 04:18:21 +0800

    Update Build_Kernel.sh

--- Build_Kernel.sh
+++ Build_Kernel.sh
@@ -56,7 +56,7 @@ sudo mv $HOME/Pixel_GKI/repo /usr/local/bin/repo
 #Sync Generic Kernel Image Source Code
 info "Sync GKI source code"
 mkdir build_kernel && cd build_kernel
-repo init -u https://android.googlesource.com/kernel/manifest -b common-$ANDROID_VERSION-$KERNEL_VERSION-$SUB_LEVEL --depth=1
+repo init -u https://android.googlesource.com/kernel/manifest -b common-${ANDROID_VERSION}-${KERNEL_VERSION}-${SUB_LEVEL} --depth=1
 repo sync
 
 #Download SukiSU-Ultra
@@ -70,17 +70,17 @@ export KSU_VERSION=$KSU_VERSION
 sed -i "s/DKSU_VERSION=12800/DKSU_VERSION=${KSU_VERSION}/" kernel/Makefile
 
 #Download SUSFS
-info "Setup SUSFS"
+info "Setup SUSFS & SukiSU patch"
 cd $HOME/Pixel_GKI/build_kernel
-git clone https://gitlab.com/simonpunk/susfs4ksu.git -b gki-$ANDROID_VERSION-$KERNEL_VERSION
+git clone https://gitlab.com/simonpunk/susfs4ksu.git -b gki-${ANDROID_VERSION}-${KERNEL_VERSION}
 git clone https://github.com/SukiSU-Ultra/SukiSU_patch.git
-cp susfs4ksu/kernel_patches/50_add_susfs_in_gki-$ANDROID_VERSION-$KERNEL_VERSION ./common/
+cp susfs4ksu/kernel_patches/50_add_susfs_in_gki-${ANDROID_VERSION}-${KERNEL_VERSION} ./common/
 cp susfs4ksu/kernel_patches/fs/* ./common/fs/
 cp susfs4ksu/kernel_patches/include/linux/* ./common/include/linux/
 cd common
-sed -i 's/-32,12 +32,38/-32,11 +32,37/g' 50_add_susfs_in_gki-$ANDROID_VERSION-$KERNEL_VERSION.patch
-sed -i '/#include <trace\/hooks\/fs.h>/d' 50_add_susfs_in_gki-$ANDROID_VERSION-$KERNEL_VERSION.patch
-patch -p1 < 50_add_susfs_in_gki-$ANDROID_VERSION-$KERNEL_VERSION.patch || true
+sed -i 's/-32,12 +32,38/-32,11 +32,37/g' 50_add_susfs_in_gki-${ANDROID_VERSION}-${KERNEL_VERSION}.patch
+sed -i '/#include <trace\/hooks\/fs.h>/d' 50_add_susfs_in_gki-${ANDROID_VERSION}-${KERNEL_VERSION}.patch
+patch -p1 < 50_add_susfs_in_gki-${ANDROID_VERSION}-${KERNEL_VERSION}.patch || true
 cp ../SukiSU_patch/hooks/syscall_hooks.patch ./
 patch -p1 -F 3 < syscall_hooks.patch
 info "Complete"
