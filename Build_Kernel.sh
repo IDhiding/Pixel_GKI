bf4409c171cb5b528b545580b6d63a3f862fd185
IDhiding <24775926+IDhiding@users.noreply.github.com>
2025-06-01 07:11:45 +0800

    Update Build_Kernel.sh

--- Build_Kernel.sh
+++ Build_Kernel.sh
@@ -137,8 +137,8 @@ fi
 cd $HOME/Pixel_GKI/build_kernel || exit
 sed -i '$s|echo "\$res"|echo "\$res-$KERNEL_NAME"|' ./common/scripts/setlocalversion
 sed -i "/stable_scmversion_cmd/s/-maybe-dirty//g" ./build/kernel/kleaf/impl/stamp.bzl
+sed -i '/^CONFIG_LOCALVERSION=/ s/="\([^"]*\)"/="$KERNEL_NAME"/' ./common/arch/arm64/configs/gki_defconfig
 # sudo sed -i "s/-android14-11-g9a32439e14e9-ab13050921/$KERNEL_NAME/g" ./common/scripts/setlocalversion
-info "Kernel name : $res-$KERNEL_NAME" 
 
 #Unix timestamp converter
 info "Set kernel build time"
